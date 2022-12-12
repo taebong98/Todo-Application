374fc28bac33ea2e897f871dc7387259a7474d59
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-12 10:30:16 +0900

    start project

---
+++ src/test/java/com/taebong98/todo/Todo/App/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taebong98.todo.Todo.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
